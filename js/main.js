39500c63e6068a54f71b687ef2348cbc5fc77988
mendezrobert <mendez.robertmar@gmail.com>
2023-09-07 17:26:00 +0800

    switch

--- js/main.js
+++ js/main.js
@@ -1,5 +1,33 @@
-// Strings
-const myVariable = "Mathematics";
+// Conditionals: Switch Statements
 
-//String Methods
-console.log(myVariable.includes("mat"));
+let playerOne = "scissors";
+let computer = "rock";
+
+switch (playerOne) {
+    case computer:
+        console.log("Tie game!");
+        break;
+    case "rock":
+    if (computer === "paper") {
+        console.log("computer wins!");
+    } else {
+        console.log("playerOne wins!")
+    }
+    break;
+    case "paper":
+    if (computer === "scissors") {
+        console.log("computer wins!");
+    } else {
+        console.log("playerOne wins!")
+    }
+    break;
+    default:
+    case "scissors":
+    if (computer === "rock") {
+        console.log("computer wins!");
+    } else {
+        console.log("playerOne wins!")
+    }
+    break;
+    
+}
